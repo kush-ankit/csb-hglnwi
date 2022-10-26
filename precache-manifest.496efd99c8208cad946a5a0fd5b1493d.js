eac1b201e54f12b9a4c55ee11824cd8af1aa553c
Code Sandbox <hello@codesandbox.io>
2022-10-26 02:36:45 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.496efd99c8208cad946a5a0fd5b1493d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a92f5cf6d536b50c43b7ace1be4132a",
+    "url": "https://kush-ankit.github.io/csb-hglnwi/index.html"
+  },
+  {
+    "revision": "afe6e63d5954b890835b",
+    "url": "https://kush-ankit.github.io/csb-hglnwi/static/js/2.502d6c76.chunk.js"
+  },
+  {
+    "revision": "523b498298eba9140af1",
+    "url": "https://kush-ankit.github.io/csb-hglnwi/static/js/main.9a24601e.chunk.js"
+  },
+  {
+    "revision": "147537de1a614fb250a5",
+    "url": "https://kush-ankit.github.io/csb-hglnwi/static/js/runtime-main.9cd4b808.js"
+  }
+]);
